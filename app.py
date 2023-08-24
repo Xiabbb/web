b75aec667e61ac500e527a611bba696d8f8d9d75
Xiabbb <104492711+Xiabbb@users.noreply.github.com>
2023-08-25 07:38:17 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import binascii
 import codecs
+import time
 
 import streamlit as st
 import requests
@@ -37,9 +38,7 @@ pj = st.sidebar.selectbox(
     "请选择项目",
     ('fxpk', 'jspk', 'qppk')
 )
-
-if st.sidebar.button('登录'):
-    st.sidebar.write('登录成功！')
+st.sidebar.button('登录')
 
 login_url = 'http://139.224.72.67:8081/outletcloud/outlet-auth/app/login'
 data = {
@@ -48,6 +47,7 @@ data = {
     "projectSerial": pj
 }
 token = session.post(login_url, json=data).json()['data']['password']
+st.sidebar.success(session.post(login_url, json=data).json()['msg'])
 session.headers = {
 
     "Api-Client": "outlet-api-pushi-release",
@@ -57,17 +57,19 @@ session.headers = {
 uid_url = 'http://139.224.72.67:8081/outletcloud/outlet-api/outlet/approved/_list'
 data_uid = {
     "firstResult": 0,
-    "maxResult": 1,
+    "maxResult": 10,
     "projectSerial": pj,
     "reportTimeOrder": "DESC"
 }
 list = session.post(uid_url, json=data_uid)
+
 uids = []
 for i in list.json()['data']['list']:
-    uid = i['outletUid']
+    uidd = i['outletUid']
     taskId = i['taskId']
     reportType = i['reportType']
-    uids.append([uid, taskId, reportType])
+    uids.append([uidd, taskId, reportType])
+
 for o in uids:
     uid = o[0]
     taskId = o[1]
@@ -80,12 +82,14 @@ for o in uids:
         'outletUid': uid,
         'module': '1001'
     }
-
+    # st.sidebar.button(uid)
     url_data = 'http://139.224.72.67:8081/outletcloud/outlet-api/ubiOutletInfo/queryInteriorOutletDetail'
     info = session.post(url=url_data, data=data1).json()
     info_pc = session.post(url=url_data, data=data2).json()
-    if info['data']['ubiOutletInfoTraceDto']['name'] != '':
 
+    if len(info['data']['ubiOutletInfoTraceDto']['name']) > 1:
+
+        # st.sidebar.markdown(uid)
         paichazhaopian = []
         img = info_pc['data']['ubiOutletInfoInvDto']['outletPhotos']
         for j in img:
@@ -95,8 +99,29 @@ for o in uids:
         img = info['data']['ubiOutletInfoTraceDto']['aroundPhoto']
         for k in img:
             zhoubianhuanjingzhaopian.append(k['imageUrl'])
+        # 上报时间
+        try:
+            reportTime = info['data']['ubiOutletInfoTraceDto']['reportTime']
+        except:
+            reportTime = ''
+        # 溯源备注
+        try:
+            traceRemark = info['data']['ubiOutletInfoTraceDto']['traceRemark']
+        except KeyError:
+            traceRemark = ''
 
-        reportTime = info['data']['ubiOutletInfoTraceDto']['reportTime']
+        # 疑似排口问题
+        problemTypeValues_1 = info['data']['ubiOutletInfoTraceDto']['problemTypeValues'][0]
+        if len(info['data']['ubiOutletInfoTraceDto']['problemTypeValues']) > 1:
+            problemTypeValues_2 = info['data']['ubiOutletInfoTraceDto']['problemTypeValues'][1]
+        else:
+            problemTypeValues_2 = ''
+        problemTypeValues = problemTypeValues_1 + problemTypeValues_2
+        # 存在问题情况描述
+        if info['data']['ubiOutletInfoTraceDto']['describe'] != '':
+            describe = info['data']['ubiOutletInfoTraceDto']['describe']
+        else:
+            describe = ""
         # 排口名称
         name = info['data']['ubiOutletInfoTraceDto']['name']
         # 排查备注
@@ -142,17 +167,25 @@ for o in uids:
         # 污水来源数量
         sewageSourceNumber = info['data']['ubiOutletInfoTraceDto']['sewageSourceNumber']
 
-
-
+        # 责任主体名称
+        try:
+            unitName = info['data']['ubiOutletInfoTraceDto']['traceUnit'][0]['unitName']
+        except:
+            continue
+        # 责任主体地址
+        try:
+            address = info['data']['ubiOutletInfoTraceDto']['traceUnit'][0]['address']
+        except:
+            continue
 
         # 是否混排
         try:
             flagMixture = '否' if info['data']['ubiOutletInfoTraceDto']['flagMixture'] == '0' else '是'
         except:
             flagMixture = '错误'
-        st.markdown(f'上报人:{info["data"]["ubiOutletInfoTraceDto"]["userName"]}')
+        # st.markdown(f'上报人:{info["data"]["ubiOutletInfoTraceDto"]["userName"]}')
         st.markdown(f'排口编码: {uid}')
-        st.markdown(f'上传时间: {reportTime}')
+        st.markdown(f'上传时间: {reportTime}  上报人:{info["data"]["ubiOutletInfoTraceDto"]["userName"]}')
         # if info['data']['ubiOutletInfoTraceDto']['flagTraceDifficult'] == 1:
         #     st.title(f'是否难点: :red[是]')
         # else:
@@ -198,13 +231,11 @@ for o in uids:
         else:
             st.markdown(f'污水来源数量: {sewageSourceNumber}')
         st.text(f'------------------------------------------------------------------------')
+        st.markdown(f'溯源方式: {dictionary["名称"][int(traceMode)]}')
         if traceMethod == '错误':
-
             annotated_text('溯源手段:', (traceMethod, "", "#faf"))
-
         else:
             st.markdown(f'溯源手段: {dictionary["名称"][int(traceMethod[0])]}')
-
         st.header('排查照片')
         columns = st.columns(2)
         images = [paichazhaopian[0], paichazhaopian[1]]
@@ -253,7 +284,9 @@ for o in uids:
                     imgs.append(nodePhoto_img)
                     lon_lat.append((img_lon, img_lat))
                 nodes.append((nodeSerial, nodeType, imgs, lon_lat))
-            st.header('节点信息')
+
+            st.header(f"节点信息")
+
             for i3 in nodes:
                 columns = st.columns(len(i3[2]))
                 for j1 in range(len(columns)):
@@ -263,16 +296,46 @@ for o in uids:
                         else:
                             st.text(f'节点：{i3[0]}  类型：{dictionary["名称"][int(i3[1])]} 经纬度：有')
                         st.image(i3[2][j1])
+        # 溯源手段照片
+        traceMethodPhoto = info['data']['ubiOutletInfoTraceDto']['traceMethodPhoto']
+        trace_img = []
+        for f in range(len(traceMethodPhoto)):
+            uniPhoto_img = info['data']['ubiOutletInfoTraceDto']['traceMethodPhoto'][f][
+                'rndOssPath']
+            if uniPhoto_img == '':
+                uniPhoto_img = info['data']['ubiOutletInfoTraceDto']['traceMethodPhoto'][f][
+                    'imageUrl']
+            trace_img.append(uniPhoto_img)
+        st.header('溯源手段照片')
+        annotated_text("溯源备注:", (traceRemark, "", "#faf"))
+        # st.markdown(f'单位名称: {unitName} \n\n单位地址:{address}')
+        columns = st.columns(len(trace_img))
+        for j3 in range(len(columns)):
+            with columns[j3]:
+                st.image(trace_img[j3])
+        # 责任主体照片
+        unitPhoto_url = info['data']['ubiOutletInfoTraceDto']['traceUnit'][0]['traceUnitPhoto']
+        unis = []
+        for f in range(len(unitPhoto_url)):
+            uniPhoto_img = info['data']['ubiOutletInfoTraceDto']['traceUnit'][0]['traceUnitPhoto'][f]['rndOssPath']
+            if uniPhoto_img == '':
+                uniPhoto_img = info['data']['ubiOutletInfoTraceDto']['traceUnit'][0]['traceUnitPhoto'][f][
+                    'imageUrl']
+            unis.append(uniPhoto_img)
+        st.header('责任主体信息')
+        st.markdown(f'单位名称: {unitName} \n\n单位地址:{address}')
+        columns = st.columns(len(unis))
+        for j2 in range(len(columns)):
+            with columns[j2]:
+                st.image(unis[j2])
+
+    res = st.text_area(f'审核理由（留空即为通过）{uid}')
     # 审核
     sh_url = 'http://139.224.72.67:8081/outletcloud/outlet-api/outlet/approvalReport'
-    res = st.text_area(f'审核理由（留空即为通过）{uid}')
-    but = st.button('提交', key=uid)
-
-    if res != '':
+    if len(res) > 0:
         approvalType = 220304
     else:
         approvalType = 220305
-
     sh_data = {
         "outletUid": uid,
         "approvalType": approvalType,
@@ -282,10 +345,13 @@ for o in uids:
         "outsideSelectEntity": {},
         "taskId": taskId
     }
-    if but:
-        shenhe = session.post(url=sh_url, json=sh_data)
-        st.success(shenhe.json()['msg'])
-        st.sidebar.success(shenhe.json()['msg'])
-    re = st.sidebar.button('刷新')
-    if re:
-        st.experimental_rerun()
+
+    aa = st.button('提交', key=uid)
+    st.divider()
+    if aa:
+        ress = session.post(url=sh_url, json=sh_data)
+        st.success(ress.json()['msg'])
+        # st.experimental_rerun()
+
+
+
